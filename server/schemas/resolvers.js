126f37c514f21622e711677544c44e06d6d99966
Adam <adamgjohnson92@gmail.com>
2023-09-17 16:11:12 -0700

    set up Type Defs and resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,48 @@
+const { User, Book } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate('books');
+    },
+  },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async(parent, {authors, description, bookId, image, link, title}) => {
+      const book = await Book.create({
+        authors, description, bookId, image, link, title
+      });
+
+      await User.findOneAndUpdate(
+        {username: username},
+        {$addToSet: {books: book._id}}
+      );
+
+      return book
+    },
+
+    removeBook: async (parent, { bookId }) => {
+      return Book.findOneAndDelete({ _id: bookId });
+    },
+  },
+};
+
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 
+//I added typeDefs and resolvers pulling in from schemas. 
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
